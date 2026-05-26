4af83f58754ea0005fb9341286a686048535eeaf
Amaany <amaanybaharun.725@gmail.com>
2026-05-26 16:38:06 +0700

    setup render database

--- database.py
+++ database.py
@@ -1,24 +1,6 @@
-from sqlalchemy import create_engine
-# =========================================
-# DATABASE CONFIG
-# =========================================
-DB_USER = 'postgres'
-DB_PASSWORD = 'Project2026'
-DB_HOST = 'localhost'
-DB_PORT = '5433'
-DB_NAME = 'invoice_prediction'
+import os
 
-DATABASE_URL = (
-
-    f'postgresql://{DB_USER}:{DB_PASSWORD}'
-    f'@{DB_HOST}:{DB_PORT}/{DB_NAME}'
-
-)
-
-# =========================================
-# CREATE ENGINE
-# =========================================
-engine = create_engine(
-    DATABASE_URL
+DATABASE_URL = os.getenv(
+    'DATABASE_URL'
 )
 
